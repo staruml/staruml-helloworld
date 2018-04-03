6d1067eaa11eeeb913eb82d25ae6a446b78a0350
niklaus <niklaus.lee@gmail.com>
2018-04-03 15:06:59 +0900

    Update for StarUML V3

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, app, window */
+function handleShowMessage () {
+  window.alert('Hello, world!')
+}
 
-define(function (require, exports, module) {
-    "use strict";
+function init () {
+  app.commands.register('helloworld:show-message', handleShowMessage)
+}
 
-    var Commands       = app.getModule("command/Commands"),
-        CommandManager = app.getModule("command/CommandManager"),
-        MenuManager    = app.getModule("menu/MenuManager");
-
-    // Handler for HelloWorld command
-    function handleHelloWorld() {
-        window.alert("Hello, world!");
-    }
-
-    // Add a HelloWorld command
-    var CMD_HELLOWORLD = "tools.helloworld";
-    CommandManager.register("Hello World", CMD_HELLOWORLD, handleHelloWorld);
-
-    // Add HellWorld menu item (Tools > Hello World)
-    var menu = MenuManager.getMenu(Commands.TOOLS);
-    menu.addMenuItem(CMD_HELLOWORLD);
-
-});
+exports.init = init
